d399ff1fad39fecab7f7d9d652bb63d3f9654c6b
Michael Scherer <schmic@users.noreply.github.com>
2014-10-06 22:51:15 +0200

    added new configuration module

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+var cfg = require('config');
 var app = require('./acMonitorApp');
-var cfg = require('./config');
-var acMonitorApp = app.listen(cfg.http.port, function() {
-    console.log('Express server listening on port ' + acMonitorApp.address().port);
+
+app.listen(cfg.get('http.port'), function() {
+    console.log('Express server listening on port ' + cfg.http.port);
+    console.log('Access acMonitor at', 'http://' + cfg.http.host + (cfg.http.port !== 80 ? ':'+cfg.http.port : ''));
 });
--- libs/passport-steam.js
+++ libs/passport-steam.js
@@ -1,8 +1,8 @@
 // http://steamcommunity.com/dev
 
-var passport      = require('passport');
+var passport = require('passport');
 var SteamStrategy = require('passport-steam').Strategy;
-var cfg           = require('../config');
+var cfg = require('config');
 
 // Passport session setup.
 passport.serializeUser(function (user, done) {
@@ -15,9 +15,9 @@ passport.deserializeUser(function (obj, done) {
 
 passport.use(
     new SteamStrategy({
-            returnURL: 'http://' + cfg.http.host + ':' + cfg.http.port + '/auth/return',
-            realm: 'http://' + cfg.http.host + ':' + cfg.http.port + '/',
-            apiKey: cfg.steam.api_key
+            returnURL: 'http://' + cfg.get('http.host') + ':' + cfg.get('http.port') + '/auth/return',
+            realm: 'http://' + cfg.get('http.host') + ':' + cfg.get('http.port') + '/',
+            apiKey: cfg.get('steam.api.key')
         },
         function (identifier, profile, done) {
             profile.url = identifier;
