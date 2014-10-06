3d2b6a2ccd34365f432574b39132d02cb33171a9
Michael Scherer <schmic@users.noreply.github.com>
2014-10-06 23:45:39 +0200

    splitted navbar partial into parts

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ var cfg = require('config');
 var app = require('./acMonitorApp');
 
 app.listen(cfg.get('http.port'), function() {
-    console.log('Express server listening on port ' + cfg.http.port);
-    console.log('Access acMonitor at', 'http://' + cfg.http.host + (cfg.http.port !== 80 ? ':'+cfg.http.port : ''));
+    console.log('acMonitor running at', 'http://' + cfg.http.host + (cfg.http.port !== 80 ? ':'+cfg.http.port : ''));
 });
