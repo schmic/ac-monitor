f672b12d560a8c51fa077312f3f39330e30b601a
Michael Scherer <schmic@users.noreply.github.com>
2014-10-18 15:04:35 +0200

    fix missing database directory

--- libs/db.js
+++ libs/db.js
@@ -1,5 +1,6 @@
-var Db = require('tingodb')().Db;
-var db = new Db(__dirname + ' /../config/db', {});
+var path = require('path');
+var assert = require('assert');
+var tingoDB = require('tingodb')().Db;
 
 function addHistory(h) {
     h.tstamp = Math.round(+new Date()/1000);
@@ -9,8 +10,14 @@ function addHistory(h) {
     });
 }
 
+var dbPath = path.join(__dirname, '..', 'config', 'db', process.env.NODE_ENV || 'dev');
+require('fs-extra').ensureDir(dbPath, function(err) {
+    assert.equal(null, err);
+});
+var db = new tingoDB(dbPath, {});
+
 module.exports = {
-    database: db,
+    db: db,
     users: db.collection('users'),
     servers: db.collection('servers'),
     history: db.collection('history'),
