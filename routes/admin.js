bf7769f8f236ca4a98958d962507cb204729d800
Michael Scherer <schmic@users.noreply.github.com>
2014-10-20 22:27:28 +0200

    refactored admin views

--- routes/admin.js
+++ routes/admin.js
@@ -18,15 +18,15 @@ router.get('/', function(req, res) {
 });
 
 router.get('/index', function (req, res) {
-    var ctx = { f: 'index', session: req.session };
+    var ctx = { session: req.session };
     History.last(undefined, function(err, items) {
         ctx.history = items;
-        res.render('admin', ctx);
+        res.render('admin/index', ctx);
     });
 });
 
 router.get('/servers', function(req, res) {
-    var ctx = { f: 'servers', session: req.session };
+    var ctx = { session: req.session };
     ctx.presets = require('../libs/env').getPresetNames();
     ctx.servers = [];
     for(var presetName in ac.servers) {
@@ -39,34 +39,32 @@ router.get('/servers', function(req, res) {
             ctx.presets.splice(ctx.presets.indexOf(presetName), 1);
         }
     }
-    res.render('admin', ctx);
+    res.render('admin/servers', ctx);
 });
 
 router.get('/presets', function(req, res) {
-    var ctx = { f: 'presets', session: req.session };
+    var ctx = { session: req.session };
     ctx.presets = require('../libs/env').getPresetNames();
-    res.render('admin', ctx);
+    res.render('admin/presets', ctx);
 });
 
 router.get('/presets/:preset', function (req, res) {
     var ctx = {};
     ctx.session = req.session;
     ctx.preset = require('../libs/preset')(req.params.preset);
-    ctx.tracks = env.getTrackNames();
-    ctx.cars = env.getCarNames();
-    res.render('preset', ctx);
+    res.render('admin/preset', ctx);
 });
 
 router.get('/tracks', function(req, res) {
-    var ctx = { f: 'tracks', session: req.session };
+    var ctx = { session: req.session };
     ctx.tracks = require('../libs/env').getTrackNames();
-    res.render('admin', ctx);
+    res.render('admin/tracks', ctx);
 });
 
 router.get('/cars', function(req, res) {
-    var ctx = { f: 'cars', session: req.session };
+    var ctx = { session: req.session };
     ctx.cars = require('../libs/env').getCarNames();
-    res.render('admin', ctx);
+    res.render('admin/cars', ctx);
 });
 
 module.exports = router;
