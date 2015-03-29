bd8351278158acb3c111fc5426c1406826d690b7
Michael Scherer <schmic@users.noreply.github.com>
2015-03-29 09:50:06 +0200

    code cleanup

--- libs/socket-handler-admin.js
+++ libs/socket-handler-admin.js
@@ -1,25 +1,26 @@
 var ac = require('ac-server-ctrl');
 var History = require('../models/history');
-var contentHandler = require('./content-handler');
-var printf = require('util').format;
+var Event = require('../models/event');
+var content = require('./content-handler');
+var format = require('util').format;
 
 function deletePreset(data, cb) {
-    data.valid = contentHandler.deletePreset(data.name);
-    data.msg = printf('Preset %s %s', data.name, data.valid ? 'deleted' : 'could not be deleted');
-    console.log('admin.presets.delete', data);
+    data.valid = content.deletePreset(data.name);
+    data.msg = format('Preset %s %s', data.name, data.valid ? 'deleted' : 'could not be deleted');
     cb(data);
+    console.log('admin.presets.delete', data);
 }
 
 function validatePreset(data, cb) {
-    data.valid = contentHandler.hasPreset(data.name) ? false : true;
-    data.msg = printf('Preset %s %s', data.name, data.valid ? 'validated' : 'already exists');
-    console.log('admin.presets.validate', data);
+    data.valid = content.hasPreset(data.name) ? false : true;
+    data.msg = format('Preset %s %s', data.name, data.valid ? 'validated' : 'already exists');
     cb(data);
+    console.log('admin.presets.validate', data);
 }
 
 function savePreset(data, cb) {
-    data.valid = contentHandler.createPreset(data.name, data.content);
-    data.msg = printf('Preset %s %s', data.name, data.valid ? 'created' : 'could not be created');
+    data.valid = content.createPreset(data.name, data.content);
+    data.msg = format('Preset %s %s', data.name, data.valid ? 'created' : 'could not be created');
     console.log('admin.presets.upload', data);
     cb(data);
 }
@@ -27,9 +28,9 @@ function savePreset(data, cb) {
 function startServer(socket, data, cb) {
     ac.start(data.name, function(presetName) {
         data.valid = true;
-        data.msg = printf('Preset %s %s', presetName, data.valid ? 'started' : 'could not be started');
-        console.log('admin.server.start', data);
+        data.msg = format('Preset %s %s', presetName, data.valid ? 'started' : 'could not be started');
         cb(data);
+        console.log('admin.server.start', data);
 
         var user = socket.handshake.session.passport.user || 'Nobody';
         History.add(user,  data.msg, function(err, res) {
@@ -41,9 +42,9 @@ function startServer(socket, data, cb) {
 function stopServer(socket, data, cb) {
     ac.stop(data.name, function(presetName) {
         data.valid = true;
-        data.msg = printf('Preset %s %s', presetName, data.valid ? 'stopped' : 'could not be stopped');
-        console.log('admin.server.stop', data);
+        data.msg = format('Preset %s %s', presetName, data.valid ? 'stopped' : 'could not be stopped');
         cb(data);
+        console.log('admin.server.stop', data);
 
         var user = socket.handshake.session.passport.user || 'Nobody';
         History.add(user, data.msg, function(err, res) {
@@ -53,32 +54,36 @@ function stopServer(socket, data, cb) {
 }
 
 function saveEvent(socket, data, fn) {
-    var Event = require('../models/event');
-    Event.save(data, function(err, result) {
-        if(err) console.error(err);
+    Event.save(data, function(err, event) {
+        if(err)
+            console.error(err);
         data.valid = err ? false : true;
-        data.msg = printf('Event %s %s', data.name, data.valid ? 'saved' : 'could not be saved');
+        data.msg = format('Event %s %s', data.name, data.valid ? 'saved' : 'could not be saved');
+        fn(data);
+        console.log('admin.event.save', data);
+
         var user = socket.handshake.session.passport.user || 'Nobody';
         History.add(user, data.msg, function(err, res) {
-            if(err) return console.error(err);
-            fn(data);
+            if(err)
+                console.error(err);
         });
-        console.log('admin.event.save', data);
     });
 }
 
 function removeEvent(socket, data, fn) {
-    var Event = require('../models/event');
-    Event.remove(data.id, function(err, result) {
-        if(err) console.error(err);
+    Event.remove(data.id, function(err) {
+        if(err)
+            console.error(err);
         data.valid = err ? false : true;
-        data.msg = printf('Event %s %s', data.name, data.valid ? 'removed' : 'could not be removed');
+        data.msg = format('Event %s %s', data.name, data.valid ? 'removed' : 'could not be removed');
+        fn(data);
+        console.log('admin.event.save', data);
+
         var user = socket.handshake.session.passport.user || 'Nobody';
         History.add(user, data.msg, function(err, res) {
-            if(err) return console.error(err);
-            fn(data);
+            if(err)
+                console.error(err);
         });
-        console.log('admin.event.remove', data);
     });
 }
 
